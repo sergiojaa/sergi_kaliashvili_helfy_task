31fe2a94ab258b762f64fe674bc92f7cdeb3ee4e
seriojaa <kaliashvilisergi@gmail.com>
2026-03-03 14:26:20 +0400

    setup backend with express and typescript

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("Backend is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
